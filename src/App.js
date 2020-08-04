65830d4ca67881b15accbf4fb26b4f9acbe06d1a
pranjali24 <pranjuk464@gmail.com>
2020-08-04 16:54:21 +0530

    in search  section lower and upper case both

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./styles.css";
 import InputArea from "./components/InputArea";
 import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
 import CourseContent from "./components/CourseContent";
 
 export default function App() {
@@ -18,8 +17,7 @@ export default function App() {
       <Header title="Add Course On Website" />
       <InputArea addProductList={addProduct} />
       <Header title="Added Course Details" />
-      <SearchBar productList={productArray} />
-      <CourseContent productList={productArray} />
+     <CourseContent productList={productArray} />
     </div>
   );
 }
--- src/components/CourseContent.jsx
+++ src/components/CourseContent.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Course from "./Course";
 export default function CourseContent(props) {
+  const [searchInput, setSearchInput] = useState("");
+  function findProduct(event) {
+    setSearchInput(event.target.value);
+  }
+  let searchProduct = props.productList.filter(product => {
+    return product.productName
+      .toLowerCase()
+      .includes(searchInput.toLowerCase());
+  });
   return (
-    <div className="course-content">
-      {props.productList.map((product, index) => {
-        return (
-          <Course
-            key={index}
-            productName={product.productName}
-            productPrice={product.productPrice}
-            productQuantity={product.productQuantity}
-            productImage={product.productImage}
-          />
-        );
-      })}
+    <div>
+      <div className="search-bar">
+        <input
+          type="text"
+          onChange={findProduct}
+          placeholder="Search Product"
+          className="input-field"
+        />
+      </div>
+      <div className="course-content">
+        {searchProduct.map((product, index) => {
+          return (
+            <Course
+              key={index}
+              productName={product.productName}
+              productPrice={product.productPrice}
+              productQuantity={product.productQuantity}
+              productImage={product.productImage}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
