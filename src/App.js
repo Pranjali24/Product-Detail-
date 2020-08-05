371e8176873fe66359ee0a8a8227309f4b0f7dbc
pranjali24 <pranjuk464@gmail.com>
2020-08-05 18:19:14 +0530

    update file for route in nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
+import { Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import Product from "./components/Product";
+import Contact from "./components/Contact";
+
 import "./styles.css";
-import InputArea from "./components/InputArea";
-import Header from "./components/Header";
-import CourseContent from "./components/CourseContent";
 
 export default function App() {
-  const [productArray, setProductArray] = useState([]);
-  function addProduct(newProduct) {
-    setProductArray(preProduct => {
-      return [...preProduct, newProduct];
-    });
-  }
-
   return (
-    <div className="App">
-      <Header title="Add Course On Website" />
-      <InputArea addProductList={addProduct} />
-      <Header title="Added Course Details" />
-     <CourseContent productList={productArray} />
+    <div>
+      <Nav />
+      <Route exact path="/" component={Home} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/product" component={Product} />
     </div>
   );
 }
